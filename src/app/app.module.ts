3c54d269bad6cbd355825b6873f2544997f66409
William Guilherme Dahlke <williamgdahlke@gmail.com>
2024-08-19 22:04:43 -0300

    created aluno and curso modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CursoModule } from './curso/curso.module';
+import { AlunoModule } from './aluno/aluno.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursoModule,
+    AlunoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
