0840c08af8105ba3f4a0cb83f9b56ffa03c77d00
William Guilherme Dahlke <williamgdahlke@gmail.com>
2024-09-01 11:08:21 -0300

    Added componentes editar-curso, inserir-curso, listar-curso and curso-service

--- src/app/curso/curso.module.ts
+++ src/app/curso/curso.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListarCursoComponent } from './listar-curso/listar-curso.component';
+import { InserirCursoComponent } from './inserir-curso/inserir-curso.component';
+import { EditarCursoComponent } from './editar-curso/editar-curso.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarCursoComponent,
+    InserirCursoComponent,
+    EditarCursoComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ src/app/curso/editar-curso/editar-curso.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-curso',
+  templateUrl: './editar-curso.component.html',
+  styleUrl: './editar-curso.component.css'
+})
+export class EditarCursoComponent {
+
+}
