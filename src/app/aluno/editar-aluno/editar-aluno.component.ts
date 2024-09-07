67acf0463773ee65b1bf9663ef23c964303c829a
William Guilherme Dahlke <williamgdahlke@gmail.com>
2024-09-07 10:38:00 -0300

    removed comments in editar-aluno.component.ts

--- src/app/aluno/editar-aluno/editar-aluno.component.ts
+++ src/app/aluno/editar-aluno/editar-aluno.component.ts
@@ -7,34 +7,31 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-editar-aluno',
   templateUrl: './editar-aluno.component.html',
-  styleUrl: './editar-aluno.component.css'
+  styleUrl: './editar-aluno.component.css',
 })
 export class EditarAlunoComponent implements OnInit {
-  constructor(private service : AlunoService,
-              private route : ActivatedRoute,
-              private router : Router){
-
-  }
+  constructor(
+    private service: AlunoService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+  
   ngOnInit(): void {
     let id = +this.route.snapshot.params['id'];
-    // Com o id, obtém a pessoa
     const res = this.service.buscarPorId(id);
-    if (res !== undefined)
-    this.aluno = res;
-    else
-      throw new Error ("Pessoa não encontrada: id = " + id);
+    if (res !== undefined) 
+      this.aluno = res;
+    else 
+      throw new Error('Aluno não encontrado: id = ' + id);
   }
 
-  @ViewChild('formAluno') formAluno! : NgForm;
-  aluno : Aluno = new Aluno();
+  @ViewChild('formAluno') formAluno!: NgForm;
+  aluno: Aluno = new Aluno();
 
   atualizar(): void {
-    // Verifica se o formulário é válido
     if (this.formAluno.form.valid) {
-    // Efetivamente atualiza a o aluno
-    this.service.alterar(this.aluno);
-    this.router.navigate(['/alunos']);
+      this.service.alterar(this.aluno);
+      this.router.navigate(['/alunos']);
     }
-    }
-    
+  }
 }
